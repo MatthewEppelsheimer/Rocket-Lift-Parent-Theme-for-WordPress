c63ac4152012d4f3593654cf03dd60d436b60a34
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2012-10-22 19:51:03 -0700

    No-results template brought into Fractal system

--- rocket-lift-parent-theme/fractal/fractal.base.php
+++ rocket-lift-parent-theme/fractal/fractal.base.php
@@ -12,6 +12,38 @@
 
 <?php fractal( 'base-override' ); ?>
 
+<?
+/**
+ *	TEMPLATE PARTS
+ */
+?>
+
+<?php fractal_block( 'no-results', function(){?>
+	<article id="post-0" class="post no-results not-found">
+		<header class="entry-header">
+			<h1 class="entry-title"><?php _e( 'Nothing Found', 'rocket_lift_parent_theme' ); ?></h1>
+		</header><!-- .entry-header -->
+	
+		<div class="entry-content">
+			<?php if ( is_home() ) : ?>
+	
+				<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'rocket_lift_parent_theme' ), admin_url( 'post-new.php' ) ); ?></p>
+	
+			<?php elseif ( is_search() ) : ?>
+	
+				<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'rocket_lift_parent_theme' ); ?></p>
+				<?php get_search_form(); ?>
+	
+			<?php else : ?>
+	
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'rocket_lift_parent_theme' ); ?></p>
+				<?php get_search_form(); ?>
+	
+			<?php endif; ?>
+		</div><!-- .entry-content -->
+	</article><!-- #post-0 .post .no-results .not-found -->
+<?php }); /* end 'no-results' block */ ?>
+
 <?php fractal_block( 'comments', function(){?>
 	<?php
 		/*
@@ -76,6 +108,11 @@
 	
 	</div><!-- #comments .comments-area -->
 <?php }); /* end 'comments' block */ ?>
+
+<?php
+/**
+ *	BASE TEMPLATE
+ */
 
 <?php fractal_block( 'base', function(){?><!DOCTYPE html>
 	<html <?php language_attributes(); ?>>
@@ -209,10 +246,8 @@
 						
 										<?php rocket_lift_parent_theme_content_nav( 'nav-below' ); ?>
 						
-									<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
-						
-										<?php get_template_part( 'no-results', 'index' ); ?>
-						
+									<?php elseif : ?>						
+										<?php fractal_block( 'no-results', function(){ fractal_parent(); }); ?>
 									<?php endif; ?>
 				
 								<?php }); /* end 'content' fractal block */ ?>
