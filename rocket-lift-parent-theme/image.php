e9064f0f9d20fab539d868b4ce242180c2a852e5
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2012-10-22 19:28:56 -0700

    Migrated image.php into Fractal system

---
+++ rocket-lift-parent-theme/image.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying image attachments.
+ *
+ * @package Rocket Lift Parent Theme
+ * @since Rocket Lift Parent Theme 1.0
+ */
+?>
+
+<?php fractal_template(); ?>
+
+<?php fractal( 'image' );
