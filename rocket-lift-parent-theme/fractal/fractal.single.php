88c77a2153f0fb001e635ae6ccc582c1c186394a
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2012-10-22 19:22:57 -0700

    Migrated single.php into Fractal system

---
+++ rocket-lift-parent-theme/fractal/fractal.single.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The single.php template
+ *
+ *
+ * @package Rocket Lift Parent Theme
+ * @since Rocket Lift Parent Theme 1.0
+ */
+?>
+
+<?php fractal_template(); ?>
+
+<?php fractal_block( 'content-loop-inside', function(){ ?>
+	<?php rocket_lift_parent_theme_content_nav( 'nav-above' ); ?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+	
+			<div class="entry-meta">
+				<?php rocket_lift_parent_theme_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
+	
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'rocket_lift_parent_theme' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+	
+		<footer class="entry-meta">
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$category_list = get_the_category_list( __( ', ', 'rocket_lift_parent_theme' ) );
+	
+				/* translators: used between list items, there is a space after the comma */
+				$tag_list = get_the_tag_list( '', ', ' );
+	
+				if ( ! rocket_lift_parent_theme_categorized_blog() ) {
+					// This blog only has 1 category so we just need to worry about tags in the meta text
+					if ( '' != $tag_list ) {
+						$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'rocket_lift_parent_theme' );
+					} else {
+						$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'rocket_lift_parent_theme' );
+					}
+	
+				} else {
+					// But this blog has loads of categories so we should probably display them here
+					if ( '' != $tag_list ) {
+						$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'rocket_lift_parent_theme' );
+					} else {
+						$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'rocket_lift_parent_theme' );
+					}
+	
+				} // end check for categories on this blog
+	
+				printf(
+					$meta_text,
+					$category_list,
+					$tag_list,
+					get_permalink(),
+					the_title_attribute( 'echo=0' )
+				);
+			?>
+	
+			<?php edit_post_link( __( 'Edit', 'rocket_lift_parent_theme' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post-<?php the_ID(); ?> -->
+
+	<?php rocket_lift_parent_theme_content_nav( 'nav-below' ); ?>
+
+	<?php
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() )
+			fractal_block( 'comments', function(){
+				fractal_parent();
+			}); /* end 'comments' fractal block */
+	?>
+
+<?php }); /* end 'content-loop-inside' */ ?>
+
+<?php fractal( 'base' );
---
+++ rocket-lift-parent-theme/single.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Rocket Lift Parent Theme
+ * @since Rocket Lift Parent Theme 1.0
+ */
+?>
+<?php fractal_template(); ?>
+
+<?php fractal( 'single' );
