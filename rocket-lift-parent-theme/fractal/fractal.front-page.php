3f79f8895f1c616f05a1093e14019a9ce888c152
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2012-11-02 18:03:53 -0700

    Added front-page tempate for Fractal.

---
+++ rocket-lift-parent-theme/fractal/fractal.front-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The front page template
+ *
+ *
+ * @package Rocket Lift Parent Theme
+ * @since Rocket Lift Parent Theme 1.0
+ */
+?>
+
+<?php fractal_template(); ?>
+
+<?php fractal( 'base' );
---
+++ rocket-lift-parent-theme/front-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying the home page.
+ *
+ * @package Rocket Lift Parent Theme
+ * @since Rocket Lift Parent Theme 1.0
+ */
+?>
+<?php fractal_template(); ?>
+
+<?php fractal( 'front-page' );
